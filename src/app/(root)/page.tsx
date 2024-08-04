ad2f7c13cf33760c49ebabbedcf0bd14e24b1171
MarcosvBueno <mvbueno07@gmail.com>
2024-08-04 12:09:25 -0300

    feat: add button that create a new document

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,47 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) {
+    redirect("/sign-in");
+  }
+  const documents = [];
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  )
-}
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="empty"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
 
-export default Home
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default Home;
