b6325b42a740de64d41e014829bcd90e1a60ee23
MarcosvBueno <mvbueno07@gmail.com>
2024-08-06 23:07:47 -0300

    feat: add user mention on comments

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -5,15 +5,27 @@ import {
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUser } from "@/lib/actions/user.actions";
+import { getClerkUser, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUser({ userIds });
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUser = async ({userIds}: {userIds : string[]}) => {
     try {
@@ -22,3 +23,23 @@ export const getClerkUser = async ({userIds}: {userIds : string[]}) => {
     }
 
 }
+
+export const getDocumentUsers = async ({roomId, currentUser, text}: {roomId: string, currentUser: string, text: string}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+    if(text.length) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText));
+      
+     return parseStringify(filteredUsers);
+    }
+
+    return parseStringify(users);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
