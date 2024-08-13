1341b8955a70ff66b0e63abfa5e09dcf9893a207
MarcosvBueno <mvbueno07@gmail.com>
2024-08-12 22:34:00 -0300

    feat: add user permissions to host of room

--- src/components/Collaborator.tsx
+++ src/components/Collaborator.tsx
@@ -3,13 +3,25 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import UserTypeSelector from './UserTypeSelector'
 import { Button } from './ui/button'
+import { removeCollaborator, updateDocumentAccess } from '@/lib/actions/room.actions'
 
 const Collaborator = ({roomId, creatorId, collaborator, email, user} : CollaboratorProps) => {
     const [userType, setuserType] = useState(collaborator.userType || 'viewer')
     const [loading, setloading] = useState(false)
 
-    const shareDocumentHandler = async(type: string) => {}
-    const removeCollaboratorHandler = async(email: string) => {}
+    const shareDocumentHandler = async(type: string) => {
+        setloading(true)
+        await updateDocumentAccess({roomId, 
+            email, 
+            userType: type as UserType, 
+            updatedBy: user})
+        setloading(false)
+    }
+    const removeCollaboratorHandler = async(email: string) => {
+        setloading(true)
+        await removeCollaborator({roomId, email})
+        setloading(false)
+    }
 
   return (
     <li className='flex items-center justify-between gap-2 py-3'>
@@ -17,7 +29,7 @@ const Collaborator = ({roomId, creatorId, collaborator, email, user} : Collabora
             <Image src={collaborator.avatar} alt='avatar' width={36} height={36} className='rounded-full size-9 '/>
             <div>
                 <p className='line-clamp-1 text-sm font-semibold leading-4 text-white'>
-                    {Collaborator.name}
+                    {collaborator.name}
                     <span className='text-10-regular pl-2 text-blue-100'>
                         {loading && 'updating...' }
                     </span>
--- src/lib/actions/room.actions.ts
+++ src/lib/actions/room.actions.ts
@@ -3,7 +3,7 @@
 import {nanoid} from 'nanoid';
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
-import { parseStringify } from '../utils';
+import { getAccessType, parseStringify } from '../utils';
 
 export const createDocument = async ({userId, email} : CreateDocumentParams) => {
     const roomId = nanoid();
@@ -22,7 +22,7 @@ export const createDocument = async ({userId, email} : CreateDocumentParams) => 
     const room = await liveblocks.createRoom(roomId, {
         metadata,
         usersAccesses,
-        defaultAccesses: ["room:write"],
+        defaultAccesses:[],
     })
 
     revalidatePath(`/`)
@@ -37,11 +37,11 @@ export const getDocument = async ({roomId, userId}: {roomId: string, userId: str
     try {
         const room = await liveblocks.getRoom(roomId);
 
-    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    const hasAccess = Object.keys(room.usersAccesses).includes(userId);
 
-    // if(!hasAccess) {
-    //     throw new Error('You do not have access to this document');
-    // }
+    if(!hasAccess) {
+        throw new Error('You do not have access to this document');
+    }
 
     return parseStringify(room);
     } catch (error) {
@@ -72,5 +72,45 @@ export const getDocuments = async (email: string) => {
     return parseStringify(rooms);
     } catch (error) {
         console.log(error)
+    }
+}
+
+export const updateDocumentAccess = async ({roomId, email, userType, updatedBy} : ShareDocumentParams) => {
+    try {
+        const usersAccesses: RoomAccesses = {
+            [email]: getAccessType(userType) as AccessType,
+
+        }
+        const room = await liveblocks.updateRoom(roomId, {usersAccesses})
+
+        if(room) {
+            
+        }
+        revalidatePath(`/documents/${roomId}`)
+
+        return parseStringify(room)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const removeCollaborator = async ({roomId, email} : {roomId: string, email: string}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId)
+
+        if(room.metadata.email === email) {
+            throw new Error('You cannot remove the owner of the document')
+        } 
+
+        const updatedRoom = await liveblocks.updateRoom(roomId, {
+            usersAccesses: {
+                [email]: null
+            }
+        })
+
+        revalidatePath(`/documents/${roomId}`)
+        return parseStringify(updatedRoom)
+    } catch (error) {
+    console.log(error)  
     }
 }
